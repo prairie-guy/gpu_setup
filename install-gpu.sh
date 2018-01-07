db070dbe162f669be22093e83db184e680d72961
C. Bryan Daniels <cdaniels@nandor.net>
2018-01-07 10:52:20 -0600

    New version of install-gpu.sh to work with v2 of fast.ai course. Upgrades to Python 3.6, Keras 2.0, etc.

--- install-gpu.sh
+++ install-gpu.sh
@@ -1,68 +1,109 @@
 # CBD: Basic Default setup. See: sodium:~/sysadmin/silver/setup_gpu/install-gpu.sh
+# See: Modified from http://files.fast.ai/setup/paperspace
 #
-# Install a cuda, nvida gpu capable machine
+# Install a cuda, nvida gpu capable machine for use with fast.ai
+# Install a generally useful environment
 # This script is designed to work with ubuntu 16.04 LTS
+
+# USE: git clone https://github.com/prairie-guy/gpu_setup.git
+#     ./gpu_setup.sh
+
+set -e
+set -o xtrace
+DEBIAN_FRONTEND=noninteractive
 
 # Ensure system is updated and has basic build tools
 sudo apt-get update
+sudo apt install unzip -y
+sudo apt -y upgrade --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+sudo apt -y install --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" qtdeclarative5-dev qml-module-qtquick-controls
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt update
 sudo apt-get --assume-yes upgrade
+sudo apt -y autoremove
+
+# Install openssh-server
+sudo apt-get --assume-yes install openssh-server
+
+# Add some key packages
 sudo apt-get --assume-yes install tmux build-essential gcc g++ make binutils emacs24-nox git zip software-properties-common
 
-# Install Anaconda for current user
+# Create local download dir
 mkdir downloads
-cd downloads
-wget "https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh" -O "Anaconda2-4.3.1-Linux-x86_64.sh"
-bash "Anaconda2-4.3.1-Linux-x86_64.sh" -b
-echo "export PATH=\"$HOME/anaconda2/bin:\$PATH\"" >> ~/.bashrc
-export PATH="$HOME/anaconda2/bin:$PATH"
-conda install -y bcolz
-conda upgrade -y --all
 
-# Set Up nvida and cuda
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb -O cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-sudo apt-get update
-sudo apt-get -y install cuda
-sudo apt-get update
+# Install cuda and cudnn
+cd ~/downloads/
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt update
+sudo apt install cuda -y
+
+wget http://files.fast.ai/files/cudnn-9.1-linux-x64-v7.tgz
+tar xf cudnn-9.1-linux-x64-v7.tgz
+sudo cp cuda/include/*.* /usr/local/cuda/include/
+sudo cp cuda/lib64/*.* /usr/local/cuda/lib64/
+
 sudo modprobe nvidia
 nvidia-smi
+cd
 
-# Install cudnn libraries
-wget "http://files.fast.ai/files/cudnn.tgz" -O "cudnn.tgz"
-tar -zxf cudnn.tgz
-cd cuda
-sudo cp lib64/* /usr/local/cuda-8.0/lib64/
-sudo cp include/* /usr/local/cuda-8.0/include/
-rm -fr cuda
+# Inatall Anaconda
+cd ~/downloads/
+wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh -b
+echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
+export PATH=~/anaconda3/bin:$PATH
+conda upgrade -y --all
+cd
 
-# Install and configure theano
-pip install theano
-echo "[global]
-device = gpu
-floatX = float32
+# Clone the fast.ai course repo
+# Importantly, this creates a default conda environment by 'source activate fasti' which is added to .bashrc
+# !!! USE conda vs. pip for package management !!!
+git clone https://github.com/fastai/fastai.git
+cd fastai/
+conda env update
+echo 'source activate fastai' >> ~/.bashrc
+source activate fastai
+source ~/.bashrc
+cd
 
-[cuda]
-root = /usr/local/cuda" > ~/.theanorc
+## Jupyter Setup
+jupyter notebook --generate-config
+jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
+echo "c.NotebookApp.password = u'"$jupass"'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
 
-# Install and configure keras
-pip install keras # Oringially: pip install keras==1.2.2 (LOOK AT THIS TO SEE IF ANYTHING BREAKS)
-mkdir ~/.keras
-echo '{
-    "image_dim_ordering": "th",
-    "epsilon": 1e-07,
-    "floatx": "float32",
-    "backend": "theano"
-}' > ~/.keras/keras.json
+# Add Jupyter Notebook Extensions, most importantly "Select CodeMirror Keymap" allows for emacs mode.
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+pip install jupyter_contrib_nbextensions
+conda install -c conda-forge jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
 
-# Clone the fast.ai course repo and prompt to start notebook
-cd ~
-git clone https://github.com/fastai/courses.git
-echo "\"jupyter notebook\" will start Jupyter on port 8888"
-echo "If you get an error instead, try restarting your session so your $PATH is updated"
-ls
+# Install and Configure Julia
+# EDIT FOR MOST RECENT VERSION OF JULIA
+JULIA_VERSION=julia-0.6.2-linux-x86_64.tar.gz
+cd ~/downloads/
+wget https://julialang-s3.julialang.org/bin/linux/x64/0.6/$JULIA_VERSION
+tar xfv $JULIA_VERSION
+rm $JULIA_VERSION
+mv julia* ~/.
+echo 'export PATH="/home/cdaniels/julia/bin:$PATH"'  >> ~/.bashrc
+cd
 
-# Kaggle 
-pip install kaggle-cli
+# Allow Julia to work on jupyter
+# If upgrading julia, install new version of julia and remove prior install of IJulia: rm -fr /home/cdaniels/.julia/v0.6/IJulia/
+julia -e 'Pkg.update()'
+julia -e 'Pkg.add("IJulia")'
+
+# Install Clojure for Juypter
+git clone https://github.com/roryk/clojupyter
+cd clojupyter
+make
+make install
+cd
 
 # Set up dotfile
 mkdir ~/.ssh
@@ -73,44 +114,17 @@ cp -r dot_files/* ~/.
 git clone git@github.com:prairie-guy/emacs_dotfile.git .emacs.d    
 cd .emacs.d
 ./setup.sh
-cd ..
+cd
 
-## Jupyter Setup
-# Configure jupyter and prompt for password
-jupyter notebook --generate-config
-jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
-echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
+# Kaggle 
+pip install kaggle-cli
 
-# Add Jupyter Notebook Extensions, most importantly "Select CodeMirror Keymap" allows for emacs mode.
-pip install jupyter_contrib_nbextensions
-conda install -c conda-forge jupyter_contrib_nbextensions
-jupyter contrib nbextension install --user
-
-
-# Install and Configure Julia
-# EDIT FOR MOST RECENT VERSION OF JULIA
-JULIA_VERSION=julia-0.6.2-linux-x86_64.tar.gz
-
-wget https://julialang-s3.julialang.org/bin/linux/x64/0.6/$JULIA_VERSION
-mkdir tmp
-tar xfv $JULIA_VERSION -C tmp
-mv tmp/julia* julia
-rm -fr tmp
-
-# Allow Julia to work on jupyter
-# If upgrading julia, install new version of julia and remove prior install of IJulia: rm -fr /home/cdaniels/.julia/v0.6/IJulia/
-julia -e 'Pkg.update()'
-julia -e 'Pkg.add("IJulia")'
-
-# Export Julia to PATH
-echo 'export PATH="/home/cdaniels/julia/bin:$PATH"'  >> ~/.bashrc
-
-# Install Clojure for Juypter
-git clone https://github.com/roryk/clojupyter
-cd clojupyter
-make
-make install
-
-
+# Add specific fast.ai course files
+cd
+mkdir data
+cd data
+wget http://files.fast.ai/data/dogscats.zip
+unzip -q dogscats.zip
+cd ../fastai/courses/dl1/
+ln -s ~/data ./
+cd
