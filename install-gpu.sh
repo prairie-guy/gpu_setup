e29ea2caf6667453ec0c2e1c509fc17494cdaff5
C. Bryan Daniels <cdaniels@nandor.net>
2018-01-07 12:32:50 -0600

    Tweaks

--- install-gpu.sh
+++ install-gpu.sh
@@ -54,30 +54,24 @@ wget http://files.fast.ai/files/cudnn-9.1-linux-x64-v7.tgz
 tar xf cudnn-9.1-linux-x64-v7.tgz
 sudo cp cuda/include/*.* /usr/local/cuda/include/
 sudo cp cuda/lib64/*.* /usr/local/cuda/lib64/
-
-sudo modprobe nvidia
-nvidia-smi
 cd
 
-# Inatall Anaconda
+# Install Anaconda and fastai
 cd ~/downloads/
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 bash Anaconda3-5.0.1-Linux-x86_64.sh -b
-echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
-export PATH=~/anaconda3/bin:$PATH
-conda upgrade -y --all
 cd
 
-# Clone the fast.ai course repo
-# Importantly, this creates a default conda environment by 'source activate fasti' which is added to .bashrc
-# !!! USE conda vs. pip for package management !!!
 git clone https://github.com/fastai/fastai.git
 cd fastai/
+echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
+export PATH=~/anaconda3/bin:$PATH
+source ~/.bashrc
 conda env update
 echo 'source activate fastai' >> ~/.bashrc
 source activate fastai
 source ~/.bashrc
-cd
+cd 
 
 ## Jupyter Setup
 jupyter notebook --generate-config
@@ -93,28 +87,30 @@ pip install jupyter_contrib_nbextensions
 conda install -c conda-forge jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
 
+
 # Install and Configure Julia
 # EDIT FOR MOST RECENT VERSION OF JULIA
 JULIA_VERSION=julia-0.6.2-linux-x86_64.tar.gz
-cd ~/downloads/
-wget https://julialang-s3.julialang.org/bin/linux/x64/0.6/$JULIA_VERSION
-tar xfv $JULIA_VERSION
-rm $JULIA_VERSION
-mv julia* ~/.
-echo 'export PATH="/home/cdaniels/julia/bin:$PATH"'  >> ~/.bashrc
 cd
+wget https://julialang-s3.julialang.org/bin/linux/x64/0.6/$JULIA_VERSION
+mkdir tmp
+tar xfv $JULIA_VERSION -C tmp
+mv tmp/julia* julia
+rm -fr tmp
 
 # Allow Julia to work on jupyter
 # If upgrading julia, install new version of julia and remove prior install of IJulia: rm -fr /home/cdaniels/.julia/v0.6/IJulia/
 julia -e 'Pkg.update()'
 julia -e 'Pkg.add("IJulia")'
 
+# Export Julia to PATH
+echo 'export PATH="/home/cdaniels/julia/bin:$PATH"'  >> ~/.bashrc
+
 # Install Clojure for Juypter
 git clone https://github.com/roryk/clojupyter
 cd clojupyter
 make
 make install
-cd
 
 # Kaggle 
 pip install kaggle-cli
