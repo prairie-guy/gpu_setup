e5e4a089e6bb02475dcd140de813378bd7660c08
praire-guy <cdaniels@nandor.net>
2018-01-30 18:23:35 -0600

    add

--- install-gpu.sh
+++ install-gpu.sh
@@ -16,10 +16,10 @@ set -o xtrace
 DEBIAN_FRONTEND=noninteractive
 
 # Install personal dotfiles for bash and ssh
-rm -fr .ssh downloads data bin tmp scratch projects 
-cp -rf dot_files/.* ~/.
+rm -fr .ssh downloads data bin tmp scratch projects
+cp -rf gpu_setup/dot_files/.* .
 mkdir ~/.ssh
-cp -fauthorized_keys ~/.ssh/.
+cp -f gpu_setup/authorized_keys ~/.ssh/.
 cd
 
 # Ensure system is updated and has basic build tools
