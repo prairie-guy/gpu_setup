427cfdbb08ad03cf8457c810f79ce95e84cd3bd2
prairie-guy <cdaniels@nandor.net>
2019-06-23 16:55:07 -0500

    Added conda-express as a light-weight version of install-fastai. Not strictly needed.

--- conda-express.sh
+++ conda-express.sh
@@ -1,50 +1,32 @@
 #!/usr/bin/env bash
 
-## 2nd Part of gpu_setup
-## Install fastai library, course and conda requirements
 ## Make sure your have previously installed fastai env with:
 ## ``conda create -n fastai python=3.6``
 ## SHOULD BE IN FASTAI ENV BEFORE RUNNING SCRIPT!!!
 
-if  `env|grep CONDA_DEFAULT|grep fastai`  ; 
-then echo env=fastai; 
-else echo conda is not env=fastai, exiting...; exit;
-fi
 
-cd
-rm -fr fastai course-v3 ai_utilities
-
-## Add fastai to PATH
-cd
-echo export PYTHONPATH=$PYTHONPATH:~/fastai >> ~/.bashrc
-export PYTHONPATH=$PYTHONPATH:~/fastai
+### Lighter Install of install-fastai.sh. Quicker
 
 ## Early Installs - To avoid problems with Pillow
 conda install -c hellock icrawler
 pip install python-magic
 
-## Install fast.ai
-# Install fast.ai cuda90 module is tied to version of Linux version of cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+# Quick and Dirty fastai express install. NOT a replacment for: install-fastai.sh 
 cd
-git clone https://github.com/fastai/fastai.git
 conda install -c pytorch -c fastai fastai pytorch torchvision cudatoolkit=10.0
 
 ## Optimize (Note pillow-simd is testing)
 conda uninstall -y --force pillow libjpeg-turbo
 conda install -c fastai/label/test pillow-simd
 
-## Late Installs
-conda install ipython
-
-## Install course-v3
-cd
-git clone https://github.com/fastai/course-v3.git
-
 ## Jupyter Setup
 conda install jupyter notebook
 conda install -c conda-forge jupyter_contrib_nbextensions
 
-## This was installed in gpu_install.sh and is not necesarily needed
+## Late Installs
+conda install ipython
+
+## This was installed in gpu_install.sh
 jupyter notebook --generate-config
 jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
 echo "c.NotebookApp.password = u'"$jupass"'" >> ~/.jupyter/jupyter_notebook_config.py
@@ -55,12 +37,6 @@ echo "c.NotebookApp.allow_remote_access = True" >> ~/.jupyter/jupyter_notebook_c
 ## Kaggle
 pip install kaggle
 
-## Set up ai_utilities
-cd
-git clone https://github.com/prairie-guy/ai_utilities.git
-cd ai_utilities
-cd
-
 ## Check Status of Optimizations
 python -c "import fastai.utils; fastai.utils.check_perf()"
 python -c "from PIL import Image; print(Image.PILLOW_VERSION)"
@@ -70,6 +46,6 @@ python -c "from PIL import features; print(features.check_feature('libjpeg_turbo
 python -c 'import fastai.utils; fastai.utils.show_install(1)'
 
 ## Might as well install R
-conda install -c r r-essentials
+# conda install -c r r-essentials
 
 echo "Sucess: fastai_setup.sh installed"
