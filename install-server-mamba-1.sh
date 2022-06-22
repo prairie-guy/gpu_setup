6835be19dab9f54db9e2d677f631d2e1e6248d82
C. Bryan Daniels <cdaniels@carbon.nandor.net>
2022-06-22 00:58:59 -0500

    Tweaking mamba

--- install-server-mamba-1.sh
+++ install-server-mamba-1.sh
@@ -9,7 +9,7 @@
 
 ## Clearn up
 cd
-rm -fr mambaforge*
+rm -fr mambaforge* Mambaforge*
 
 ## Mamba
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
